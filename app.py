7912a12a463e78dd8c8f0133ebe58a881b0b1437
acharapornpptns-cyber <acharaporn.pptns@g.swu.ac.th>
2026-05-19 21:24:01 +0700

    add app.py and requirement.txt

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Cancer Classification App")
+
+st.write("Innovative Deep Learning Architecture for the Classification of Lung and Colon Cancer From Histopathology Images")
